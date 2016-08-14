fd727c22dae0ada1c798bbbb58b110965287981c
LordwolF76 <lordwolf76@gmail.com>
2016-08-13 22:06:03 -0700

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,23 @@
+/**
+ * Created by LordwolF on 8/13/2016.
+ */
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [Name=" + name + "]";
+    }
+}
